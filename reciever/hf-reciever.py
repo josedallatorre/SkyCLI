e61c25727869f61f4608abba9ab8bc99a4abe299
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-17 14:15:58 +0200

    feat: adding high freq reciever basic func

---
+++ reciever/hf-reciever.py
@@ -0,0 +1,10 @@
+from Sky-CLI.LoRa-RaspberryPi import loralib                                                          
+import time
+
+loralib.init(1, 868000000, 7)
+
+for i in range(0,10000000):
+  msg=loralib.recv()
+  if msg[5] == 0 and msg[1] > 0:
+    print(msg)
+  time.sleep(0.001)
