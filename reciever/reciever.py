f3366d1ec2b2cb1eb366f5c543caf95e9ec98a9a
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-17 14:15:47 +0200

    feat: adding reciever basic func

---
+++ reciever/reciever.py
@@ -0,0 +1,10 @@
+from Sky-CLI.LoRa-RaspberryPi import loralib                                                          
+import time
+
+loralib.init(1, 868000000, 7)
+
+for i in range(0,10000000):
+  msg=loralib.recv()
+  print("%06d, frame=" % i, end='')
+  print(msg)
+  time.sleep(1)
