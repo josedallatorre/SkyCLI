d30a9958446ddec297ad4157f5c39c2f266c0f6c
josedallatorre <jose.dallatorre@hotmail.com>
2025-07-17 14:26:43 +0200

    fix: ";"

--- sender/sender.py
+++ sender/sender.py
@@ -1,3 +1,3 @@
 from LoRaRaspberryPi import loralib                                                          
 loralib.init(0, 868000000, 7)                                           
-loralib.send(b'hello');
+loralib.send(b'hello')
