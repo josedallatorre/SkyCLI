8ab58685ad4df49efb8a77bfde47bbf233617fa4
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-13 21:45:03 +0200

    feat: added loggin to hf sender

--- sender/hf-sender.py
+++ sender/hf-sender.py
@@ -1,7 +1,17 @@
 from LoRaRaspberryPi import loralib                                                          
 import time
+from utils.logging import create_logger
 
 loralib.init(0, 868000000, 7)
+
+logger = create_logger('hf-sender')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
 
 for i in range(0,1000):
     loralib.send(b'hello')
