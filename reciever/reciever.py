67983aa3809d5dbcf532462efaec4883e9b5ee07
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-13 15:34:37 +0200

    feat: logging now works

--- reciever/reciever.py
+++ reciever/reciever.py
@@ -1,11 +1,19 @@
-from LoRaRaspberryPi import loralib                                                          
+#from LoRaRaspberryPi import loralib                                                          
+from datetime import datetime
 import logging 
 import time
+from pathlib import Path
+
+#create log directory
+Path("./log/reciever").mkdir(exist_ok=True, parents=True)
 
 #create logger
-logging.basicConfig(filename='./log/%d-%m-%y.log', filemode='w')
 logger = logging.getLogger('reciever')
 logger.setLevel(logging.DEBUG)
+
+
+fh = logging.FileHandler(filename='./log/reciever/{:%Y-%m-%d}.log'.format(datetime.now()), mode='w')
+fh.setLevel(logging.DEBUG)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
@@ -15,9 +23,11 @@ ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # add formatter to ch
+fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
 # add ch to logger
+logger.addHandler(fh)
 logger.addHandler(ch)
 
 # 'application' code
@@ -27,6 +37,7 @@ logger.warning('warn message')
 logger.error('error message')
 logger.critical('critical message')
 
+"""
 loralib.init(1, 868000000, 7)
 
 for i in range(0,10000000):
@@ -34,3 +45,4 @@ for i in range(0,10000000):
   print("%06d, frame=" % i, end='')
   print(msg)
   time.sleep(1)
+  """
