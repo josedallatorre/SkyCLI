2b1d21c8c0c589ae13dcc5b4c2315b801e650122
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-13 14:57:42 +0200

    feat: basic logging

--- reciever/reciever.py
+++ reciever/reciever.py
@@ -1,5 +1,31 @@
 from LoRaRaspberryPi import loralib                                                          
+import logging 
 import time
+
+#create logger
+logging.basicConfig(filename='./log/%d-%m-%y.log')
+logger = logging.getLogger('reciever')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
 
 loralib.init(1, 868000000, 7)
 
