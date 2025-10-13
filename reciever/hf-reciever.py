26dfd612665ffda9b76282b0dde020071adc3e3f
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-13 15:53:15 +0200

    feat: added loggin to hf reciever

--- reciever/hf-reciever.py
+++ reciever/hf-reciever.py
@@ -1,7 +1,17 @@
 from LoRaRaspberryPi import loralib                                                          
 import time
+from utils.logging import create_logger
 
 loralib.init(1, 868000000, 7)
+
+logger = create_logger('hf-reciever')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
 
 for i in range(0,10000000):
   msg=loralib.recv()
