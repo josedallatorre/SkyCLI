ab9ab949c5340c25f1301a932c7368719c962f26
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-13 21:44:50 +0200

    feat: added loggin to sender

--- sender/sender.py
+++ sender/sender.py
@@ -1,3 +1,14 @@
 from LoRaRaspberryPi import loralib                                                          
-loralib.init(0, 868000000, 7)                                           
+from utils.logging import create_logger
+
+loralib.init(0, 868000000, 7)
+logger = create_logger('sender')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
 loralib.send(b'hello')
