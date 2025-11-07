31a3a72e61a5a1029a356c2f5d5579547eb8ba89
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-07 14:24:56 +0100

    feat: adding logging to sender + counter

--- sender/sender.py
+++ sender/sender.py
@@ -1,4 +1,5 @@
 from LoRaRaspberryPi import loralib                                                          
+import time
 from utils.logging import create_logger
 
 loralib.init(0, 868000000, 7)
@@ -11,4 +12,7 @@ logger.warning('warn message')
 logger.error('error message')
 logger.critical('critical message')
 
-loralib.send(b'hello')
+for i in range(0,10000000):
+    loralib.send(f"{i}: hello".encode())
+    logger.info(f"Sent message {i}: hello")
+    time.sleep(1)
