30496323a92ce45c270821b9fc9e6c4c043452f3
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-07 15:03:38 +0100

    testing bytes conversion

--- sender/hf-sender.py
+++ sender/hf-sender.py
@@ -6,7 +6,16 @@ loralib.init(0, 868000000, 7)
 
 logger = create_logger('hf-sender')
 
+counter = [0, 0, 0]
+
 for i in range(0,10000000):
-    loralib.send(f"{i}: hello".encode())
+    temp = i
+    counter[2] = i // 255
+    temp -= counter[2] * 255
+    counter[1] = i // (255**2) 
+    temp -= counter[1] * (255**2)
+    counter[0] = i // (255**3)
+    temp -= counter[0] * (255**3)
+    loralib.send(b'counter:' + bytes(counter) + b': hello')
     logger.info(f"Sent message {i}: hello")
     time.sleep(0.001)
