0d6ee8d81762a6a105b12eb4eb3188994e0b97e7
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-07 14:24:51 +0100

    feat: adding logging to hf-sender + counter

--- sender/hf-sender.py
+++ sender/hf-sender.py
@@ -6,13 +6,7 @@ loralib.init(0, 868000000, 7)
 
 logger = create_logger('hf-sender')
 
-# 'application' code
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
-
-for i in range(0,1000):
-    loralib.send(b'hello')
+for i in range(0,10000000):
+    loralib.send(f"{i}: hello".encode())
+    logger.info(f"Sent message {i}: hello")
     time.sleep(0.001)
