5f1b6828d08f59adcaa4bf26d6789f08211a3a80
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-24 15:31:24 +0100

    feat:  using conversion function

--- sender/sender.py
+++ sender/sender.py
@@ -1,24 +1,16 @@
 from LoRaRaspberryPi import loralib                                                          
 import time
 from utils.logging import create_logger
+from utils.convertion import toDigits
 
 loralib.init(0, 868000000, 7)
 logger = create_logger('sender')
 
-counter = [0, 0, 0]
 
-for i in range(0,12000):
+for i in range(1,12000):
     temp = i
-    #TODO: substitute the magic number with index of 
-    #the list
-    counter[0] = temp // (255**2)
-    temp -= counter[0] * (255**2)
-    counter[1] = temp // (255**1) 
-    temp -= counter[1] * (255**1)
-    counter[2] = temp 
-    temp -= counter[2] 
-    assert temp == 0
-    print(counter, temp, i)
-    loralib.send(b'counter:' + bytes(counter) + b': hello')
-    logger.info(f"Sent message {i}: hello")
+    digits = toDigits(temp, 255)
+    print(digits, temp, i)
+    loralib.send(b'counter:' + bytes(digits) + b': hello')
+    logger.info(f"Sent message {i} in digits {digits}: hello")
     time.sleep(0.01)
