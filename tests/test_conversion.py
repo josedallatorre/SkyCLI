5fed1012adc3b41952dc3d8a2cfb76e9cf57dc26
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-24 15:32:14 +0100

    fix: typo in filename

---
+++ tests/test_conversion.py
@@ -0,0 +1,45 @@
+from utils.conversion import toDigits, fromDigits
+import pytest
+
+class TestConversion:
+    def test_bideractional_conversion(self):
+        for i in range(1, 100000):
+            value = i
+            digits = toDigits(value, 255)
+            print(f"value: {value}, digits: {digits}")
+            assert fromDigits(digits, 255) == value
+
+    def test_zero_toDigits(self):
+        assert toDigits(0, 2) == [0]
+    
+    def test_negative_number_toDigits(self):
+        with pytest.raises(ValueError):
+            toDigits(-5, 2)
+            
+    def test_invalid_base_fromDigits(self):
+        with pytest.raises(ValueError):
+            fromDigits([1,0,1], 1)
+    
+    def test_zero_base_fromDigits(self):
+        with pytest.raises(ValueError):
+            fromDigits([1,0,1], 0)
+    
+    def test_negative_base_fromDigits(self):
+        with pytest.raises(ValueError):
+            fromDigits([1,0,1], -2)
+
+    def test_example(self):
+        value = 42
+        assert toDigits(value, 2) == [1, 0, 1, 0, 1, 0]
+        
+    def test_one(self):
+        value = 42
+        assert toDigits(value,3) == [1, 1, 2, 0]
+ 
+    def test_two(self):
+        value = [1, 0, 1, 0, 1, 0]
+        assert fromDigits(value, 2) == 42
+
+    def test_three(self):
+        value = [1, 1, 2, 0]
+        assert fromDigits(value, 3) == 42
