95f1e25de71cdea77b5a4cf617eedd397a1eb5c4
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-24 15:36:17 +0100

    feat: updated high freq sender

--- sender/hf-sender.py
+++ sender/hf-sender.py
@@ -1,21 +1,16 @@
-from LoRaRaspberryPi import loralib                                                          
+from LoRaRaspberryPi import loralib
 import time
 from utils.logging import create_logger
+from utils.conversion import toDigits
 
 loralib.init(0, 868000000, 7)
-
 logger = create_logger('hf-sender')
 
-counter = [0, 0, 0]
 
 for i in range(0,10000000):
     temp = i
-    counter[0] = i // (255**3)
-    temp -= counter[0] * (255**3)
-    counter[1] = i // (255**2) 
-    temp -= counter[1] * (255**2)
-    counter[2] = i // 255
-    temp -= counter[2] * 255
-    loralib.send(b'counter:' + bytes(counter) + b': hello')
-    logger.info(f"Sent message {i}: hello")
+    digits = toDigits(temp, 255)
+    print(digits, temp, i)
+    loralib.send(b'counter:' + bytes(digits) + b': hello')
+    logger.info(f"Sent message {i} in digits {digits}: hello")
     time.sleep(0.001)
