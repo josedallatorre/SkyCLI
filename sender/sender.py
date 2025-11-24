3fdf9ec94cfbf36158ab6f5548eea5baf2755a0d
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-24 15:46:02 +0100

    update

--- sender/sender.py
+++ sender/sender.py
@@ -6,10 +6,10 @@ from utils.conversion import toDigits
 loralib.init(0, 868000000, 7)
 logger = create_logger('sender')
 
-
 for i in range(0,12000):
     temp = i
-    digits = toDigits(temp, 255)
+    # since lora send bytes of data we need to convert the counter to a list of digits in base 255
+    digits = toDigits(temp, 255) 
     print(digits, temp, i)
     loralib.send(b'counter:' + bytes(digits) + b': hello')
     logger.info(f"Sent message {i} in digits {digits}: hello")
