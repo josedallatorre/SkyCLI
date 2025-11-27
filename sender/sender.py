344d9cc7012a0f6f553555f96d53490b334dab32
josedallatorre <jose.dallatorre@hotmail.com>
2025-11-27 14:08:53 +0100

    feat: printing size of msg

--- sender/sender.py
+++ sender/sender.py
@@ -2,6 +2,7 @@ from LoRaRaspberryPi import loralib
 import time
 from utils.logging import create_logger
 from utils.conversion import toDigits
+import sys
 
 loralib.init(0, 868000000, 7) # init LoRa in sender mode, freq 868MHz, spread factor 7
 logger = create_logger('sender')
@@ -11,6 +12,8 @@ for i in range(0,12000):
     # since lora send bytes of data we need to convert the counter to a list of digits in base 255
     digits = toDigits(temp, 255) 
     print(digits, temp, i)
-    loralib.send(b'counter:' + bytes(digits) + b': hello')
+    msg = b'counter:' + bytes(digits) + b': hello'
+    loralib.send(msg)
+    print(sys.getsizeof(msg))
     logger.info(f"Sent message {i} in digits {digits} in bytes {bytes(digits)}: hello")
     time.sleep(0.01)
